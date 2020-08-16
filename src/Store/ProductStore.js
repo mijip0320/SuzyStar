4ebc4838ac0d096f4df4a5d7f0d1d95fe3f614ee
souvenir <mongkey0@naver.com>
2020-08-17 00:33:22 +0900

    Modify store

--- src/Store/ProductStore.js
+++ src/Store/ProductStore.js
@@ -1,19 +1,8 @@
 import { observable, computed } from "mobx";
 import Products from "../Products";
-import Users from "../Users";
-class Store {
-  @observable users = Users;
-  @observable user = {};
-
+class ProductStore {
   @observable products = Products;
   @observable product = {};
-
-  @computed get getUsers() {
-    return this.users;
-  }
-  @computed get getUser() {
-    return this.user;
-  }
 
   @computed get getProducts() {
     return this.products;
@@ -22,4 +11,4 @@ class Store {
     return this.product;
   }
 }
-export default new Store();
+export default new ProductStore();
